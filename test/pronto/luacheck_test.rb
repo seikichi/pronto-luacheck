85dd9acd2993f408433e0198d56ea0b37b8d97fa
Seiichi KONDO <seikichi@kmc.gr.jp>
2016-05-15 17:33:14 +0900

    Remove unnecessary test

--- test/pronto/luacheck_test.rb
+++ test/pronto/luacheck_test.rb
@@ -5,9 +5,5 @@ module Pronto
     def test_that_it_has_a_version_number
       refute_nil ::Pronto::Luacheck::VERSION
     end
-
-    def test_it_does_something_useful
-      assert false
-    end
   end
 end
